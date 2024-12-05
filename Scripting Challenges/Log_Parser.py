ca7cfc2e2103e86a8d7f4723ab1ab6464df982ec
isantos01 <102042372+isantos01@users.noreply.github.com>
2024-12-05 02:52:34 -0400

    commit

--- Scripting Challenges/Log_Parser.py
+++ Scripting Challenges/Log_Parser.py
@@ -1,20 +1,16 @@
 # Objective: Write a script that reads a log file and identifies failed login attempts.
 
-import subprocess
-import os
+file = input("name of the log file: ")
 
-#file = input("name of the log file: ")
-path = input("path of the log file: *remember to use the full path* ")
-
-# Accessing directory
-directory = os.system("cd" + path)
-print (directory)
-
-# if subprocess.Popen == False:
-#     print("Error accessing directory")
-# else:
-#     # read the log file
-#     with open(file, 'r') as log:
-#         for line in log:
-#             if "Failed password":
-#                 print(line.strip)
+with open (file, "r") as log:
+    for line in log:
+        if "Failed password" in line:
+            # Now will print the
+            timestamp = line[0:14].strip()
+            # locate "from" in the text
+            from_location = line.find('from')
+            port_location = line.find('port')
+            for_location = line.find('for')
+            ip = line[from_location+4:port_location].strip()
+            user = line[for_location+3:from_location].strip()
+            print('Timestamp: '+ timestamp +' User(s): ' + user +' IP: '+ ip)
