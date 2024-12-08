ce5bb9bdb3b47deaf812873ea8d626acd89d41ac
isantos01 <102042372+isantos01@users.noreply.github.com>
2024-12-07 22:01:47 -0400

    commif

--- Scripting Challenges/spamremover.py
+++ Scripting Challenges/spamremover.py
@@ -7,7 +7,8 @@ new_mail = ""
 for character in email:
     # Counting the occurrance of each letter
     spam = email.count(character)
-    # If it's more than 3 times store it in the empty 
+    # If it's more than 3 times store it in the empty variable
     if spam < 3:
+        # adding each of the letter in character to the empty variable
         new_mail += character
 print(new_mail)
